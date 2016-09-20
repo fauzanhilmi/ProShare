d9a5e9f3366ff08b604df303e9fe8aa2becb0198
fauzanhilmi <fauzanhilmir@gmail.com>
2016-09-20 10:51:50 +0800

    Handled connection error

--- ProShare/Form1.cs
+++ ProShare/Form1.cs
@@ -20,12 +20,22 @@ namespace ProShare
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            AccountDatabase.Connect();
-            string uid = "tranquillo";
-            string pass = "barnetta";
-            Debug.WriteLine(AccountDatabase.Add(uid, pass));
-            //Debug.WriteLine(AccountDatabase.isExist(uid, pass));
-            AccountDatabase.CloseConnection();
+            try
+            {
+                AccountDatabase.Connect();
+                string uid = "tranquillo";
+                string pass = "barnetta";
+                Debug.WriteLine(AccountDatabase.Add(uid, pass));
+                //Debug.WriteLine(AccountDatabase.isExist(uid, pass));
+                AccountDatabase.CloseConnection();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                if(ex.Number == 1042) //Connection Error 
+                {
+                    Debug.WriteLine("Connection Error (" + ex.Number + ") : " + ex.Message);
+                }
+            }            
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,2 @@
+# ProShare
+A secret sharing application that supports shares generation, secret reconstruction, and shares updating. Implemented on Windows Form (.NET) with Visual Studio 2016.
