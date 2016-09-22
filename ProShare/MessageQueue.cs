efae6f2f91d9a6492810dc6226cdadb7be969330
fauzanhilmi <fauzanhilmir@gmail.com>
2016-09-22 23:24:31 +0800

    Setting up RabbitMQ

---
+++ ProShare/MessageQueue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace ProShare
+{
+    public static class MessageQueue
+    {
+        private static string server = "localhost";
+        private static string uid = "guest";
+        private static string pwd = "guest";
+
+        private static IConnection MQConn;
+
+        public static void Connect()
+        {
+            ConnectionFactory CF = new ConnectionFactory();
+            CF.HostName = server;
+            CF.UserName = uid;
+            CF.Password = pwd;
+
+            MQConn = CF.CreateConnection();
+        }
+    }
+}
